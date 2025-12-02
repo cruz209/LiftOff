cbf55b4ae110f467f3999aff8cba77cfef65e4d0
Christopher Cruz <150626221+cruz209@users.noreply.github.com>
2025-12-01 19:20:29 -0500

    Delete examples directory

--- examples/basic_flask_rag.py
+++
@@ -1,6 +0,0 @@
-
-from liftoff import LiftOff
-
-if __name__ == "__main__":
-    lf = LiftOff()  # relies on OPENAI_API_KEY or HUGGINGFACE_TOKEN env var
-    lf.create("Create a simple Flask RAG (retrieval augmented generation) app that can upload PDFs and ask questions about them using the OpenAI Api key.")
